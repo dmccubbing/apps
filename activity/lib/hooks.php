0bd7391b693fd93541bc6ddf81c35e6be18d4ac9
Joas Schilling <nickvergessen@gmx.de>
2014-03-05 17:37:15 +0100

    Little more clean up for hooks.php

--- activity/lib/hooks.php
+++ activity/lib/hooks.php
@@ -3,7 +3,7 @@
 /**
  * ownCloud - Activities App
  *
- * @author Frank Karlitschek
+ * @author Frank Karlitschek, Joas Schilling
  * @copyright 2013 Frank Karlitschek frank@owncloud.org
  *
  * This library is free software; you can redistribute it and/or
@@ -21,14 +21,12 @@
  *
  */
 
-
 namespace OCA\Activity;
 
 /**
  * @brief The class to handle the filesystem hooks
  */
 class Hooks {
-	
 	public static $createhookfired = false;
 	public static $createhookfile = '';
 
@@ -37,7 +35,6 @@ class Hooks {
 	 * All other events has to be triggered by the apps.
 	 */
 	public static function register() {
-
 		//Listen to create file signal
 		\OCP\Util::connectHook('OC_Filesystem', 'post_create', "OCA\Activity\Hooks", "file_create");
 
@@ -112,7 +109,6 @@ class Hooks {
 	 * @param array $params The hook params
 	 */
 	public static function file_delete($params) {
-
 		$link = \OCP\Util::linkToAbsolute('files', 'index.php', array('dir' => dirname($params['path'])));
 		$subject = '%s deleted';
 		Data::send('files', $subject, substr($params['path'], 1), '', array(), $params['path'], $link, \OCP\User::getUser(), 2);
@@ -175,11 +171,9 @@ class Hooks {
 	 * @param array $params The hook params
 	 */
 	public static function file_create($params) {
-
 		// remember the create event for later consumption
 		self::$createhookfired = true;
 		self::$createhookfile = $params['path'];
-
 	}
 
 	/**
@@ -189,9 +183,8 @@ class Hooks {
 	public static function share($params) {
 
 		if ($params['itemType'] === 'file' || $params['itemType'] === 'folder') {
-	
 			$link = \OCP\Util::linkToAbsolute('files', 'index.php', array('dir' => dirname($params['fileTarget'])));
-			$link2 = \OCP\Util::linkToAbsolute('files', 'index.php', array('dir' => dirname('/Shared/'.$params['fileTarget'])));
+			$link_shared = \OCP\Util::linkToAbsolute('files', 'index.php', array('dir' => dirname('/Shared/'.$params['fileTarget'])));
 
 			$sharedFrom = \OCP\User::getUser();
 			$shareWith = $params['shareWith'];
@@ -199,15 +192,13 @@ class Hooks {
 			if(!empty($shareWith)) {
 				$subject = 'You shared %s with %s';
 				Data::send('files', $subject, array(substr($params['fileTarget'], 1), $shareWith), '', array(), $params['fileTarget'], $link, \OCP\User::getUser(), 4, Data::PRIORITY_MEDIUM );
-			
+
 				$subject = '%s shared %s with you';
-				Data::send('files', $subject, array($sharedFrom, substr('/Shared'.$params['fileTarget'], 1)), '', array(), '/Shared/'.$params['fileTarget'], $link2, $shareWith, 5, Data::PRIORITY_MEDIUM);
+				Data::send('files', $subject, array($sharedFrom, substr('/Shared'.$params['fileTarget'], 1)), '', array(), '/Shared/'.$params['fileTarget'], $link_shared, $shareWith, 5, Data::PRIORITY_MEDIUM);
 			} else {
 				$subject = 'You shared %s';
 				Data::send('files', $subject, array(substr($params['fileTarget'], 1)), '', array(), $params['fileTarget'], $link, \OCP\User::getUser(), 4, Data::PRIORITY_MEDIUM );
 			}
-			
 		}
-
 	}
 }
